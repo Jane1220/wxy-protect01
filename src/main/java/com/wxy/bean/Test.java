b207c6002498de7d695496ca0e1baaef56896a3d
Jane1220 <1521609483@qq.com>
2019-11-14 11:24:16 +0800

    dsa

---
+++ src/main/java/com/wxy/bean/Test.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.wxy.bean;
+
+/**
+ * @作者： 王新宇
+ * @时间：2019年11月14日
+ */
+public class Test {
+	public static void main(String[] args) {
+		
+		System.out.println("helle world");
+		
+	}
+
+}
